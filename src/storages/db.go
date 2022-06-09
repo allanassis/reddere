d0dae7f521f1913092ccfbf024c3e0d0e7b6ce7f
Allan Assis <allan-bdc@hotmail.com>
2022-06-08 23:44:50 -0300

    Create save method in storage

--- src/storages/db.go
+++ src/storages/db.go
@@ -1,6 +1,7 @@
 package storages
 
 import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -17,6 +18,24 @@ type Database struct {
 type Storage interface {
 	Connect() error
 	Healthcheck() error
+	Save(document interface{}, collectionName string) (string, error)
+}
+
+func (db *Database) Save(document interface{}, collectionName string) (string, error) {
+	collection := db.client.Database("reddere").Collection(collectionName)
+
+	ctxInsert, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	insertedResult, err := collection.InsertOne(ctxInsert, document)
+	if err != nil {
+		panic(err)
+	}
+
+	stringObjectID := insertedResult.InsertedID.(primitive.ObjectID).Hex()
+
+	return stringObjectID, nil
+
 }
 
 func (db *Database) Connect() error {
